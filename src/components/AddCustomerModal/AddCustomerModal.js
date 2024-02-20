59e450e9dce9042d1543461c9fde1c51659fbb82
Sudarshan <2gi18me135@students.git.edu>
2024-02-20 16:17:03 +0530

    Done 20

--- src/components/AddCustomerModal/AddCustomerModal.js
+++ src/components/AddCustomerModal/AddCustomerModal.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import { db } from '../shared/firebase'; // Assuming you have a 'db' instance from Firebase
+import './addCustomerModal.css';
 
 // Functional component for the Add Customer Modal
 const AddCustomerModal = ({ isOpen, onRequestClose, isMobileUnique, setIsMobileUnique }) => {
@@ -15,6 +16,14 @@ const AddCustomerModal = ({ isOpen, onRequestClose, isMobileUnique, setIsMobileU
     totalCost: '',
     startDate: '',
   });
+
+  const [nameError, setNameError] = useState("");
+  const [mobileError, setMobileError] = useState("");
+  const [placeError, setPlaceError] = useState("");
+  // const [addressError, setAddressError] = useState("");
+  const [ageError, setAgeError] = useState("");
+  const [totalCostError, setTotalCostError] = useState("");
+  const [startDateError, setStartDateError] = useState("");
 
   // useEffect hook to fetch the maximum customer ID when the modal is opened
   useEffect(() => {
@@ -49,6 +58,88 @@ const AddCustomerModal = ({ isOpen, onRequestClose, isMobileUnique, setIsMobileU
   }, [isOpen]);
 
 
+  const validateInputs = () => {
+    let isValid = true;
+
+    // Name validation
+    if (!customerData.name.trim()) {
+      setNameError("Name is required");
+      isValid = false;
+    } else {
+      setNameError("");
+    }
+
+    // Mobile validation
+    if (!customerData.mobile.trim()) {
+      setMobileError("Mobile number is required");
+      isValid = false;
+    } else if (!/^\d{10}$/.test(customerData.mobile)) {
+      setMobileError("Invalid mobile number");
+      isValid = false;
+    } else {
+      setMobileError("");
+    }
+
+    // Place validation
+    if (!customerData.place.trim()) {
+      setPlaceError("Place is required");
+      isValid = false;
+    } else {
+      setPlaceError("");
+    }
+
+    // // Address validation
+    // if (!customerData.address.trim()) {
+    //   setAddressError("Address is required");
+    //   isValid = false;
+    // } else {
+    //   setAddressError("");
+    // }
+
+    // Age validation
+    if (!customerData.age.trim()) {
+      setAgeError("Age is required");
+      isValid = false;
+    } else if (isNaN(customerData.age) || parseInt(customerData.age) <= 0) {
+      setAgeError("Invalid age");
+      isValid = false;
+    } else {
+      setAgeError("");
+    }
+
+    // Total Cost validation
+    if (!customerData.totalCost.trim()) {
+      setTotalCostError("Total Cost is required");
+      isValid = false;
+    } else if (isNaN(customerData.totalCost) || parseFloat(customerData.totalCost) < 0) {
+      setTotalCostError("Invalid total cost");
+      isValid = false;
+    } else {
+      setTotalCostError("");
+    }
+
+    // Start Date validation
+    if (!customerData.startDate.trim()) {
+      setStartDateError("Start Date is required");
+      isValid = false;
+    } else {
+      setStartDateError("");
+    }
+
+    return isValid;
+  };
+
+
+
+
+
+
+
+
+
+
+
+
   // Event handler for input changes in the form
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -56,6 +147,17 @@ const AddCustomerModal = ({ isOpen, onRequestClose, isMobileUnique, setIsMobileU
       ...prevData,
       [name]: value,
     }));
+  };
+
+  // Function to reset errors
+  const resetErrors = () => {
+    setNameError('');
+    setMobileError('');
+    setPlaceError('');
+    // setAddressError('');
+    setAgeError('');
+    setTotalCostError('');
+    setStartDateError('');
   };
 
 
@@ -90,6 +192,10 @@ const AddCustomerModal = ({ isOpen, onRequestClose, isMobileUnique, setIsMobileU
   // Function to handle saving customer data to the 'customers' collection
 const handleSave = async () => {
   try {
+    resetErrors();
+    if (!validateInputs()) {
+      return;
+    }
     // Check if the mobile number is unique
     const isUnique = await checkMobileNumberUnique(customerData.mobile);
 
@@ -123,12 +229,10 @@ const handleSave = async () => {
     console.error('Error saving customer data:', error.message);
   }
 };
-  
 
   return (
     <Modal
       isOpen={isOpen}
-      onRequestClose={onRequestClose}
       contentLabel="Add Customer Modal"
       style={customStyles} // Apply the custom styles
     >
@@ -139,40 +243,49 @@ const handleSave = async () => {
       <div className='container1'>
         <label className='all-label'>Name *
         <input className='inputbox1' type="text" name="name" value={customerData.name} onChange={handleInputChange} />
+        <div className="error-messages">{nameError}</div>
         </label>
         <label  className='all-label'>Mobile Number *
         <input className='inputbox1' type="text" name="mobile" value={customerData.mobile} onChange={handleInputChange} />
         {isMobileUnique ? null : (
         <div style={{ color: 'red' }}>Mobile number must be unique</div>
-      )}     
+      )} 
+      <div className="error-messages">{mobileError}</div>    
       </label>
       </div>
 
       <div className='container1'>
         <label className='all-label'>Place *
         <input className='inputbox1' type="text" name="place" value={customerData.place} onChange={handleInputChange} />
+        <div className="error-messages">{placeError}</div>  
         </label>
-        <label className='all-label'>Address *
+        <label className='all-label'>Address
         <input className='inputbox1' type="text" name="address" value={customerData.address} onChange={handleInputChange} />
+        {/* <div className="error-messages">{addressError}</div>   */}
         </label>
       </div>
 
       <div className='container1'>
         <label className='all-label'>Age *
         <input className='inputbox1' type="text" name="age" value={customerData.age} onChange={handleInputChange} />
+        <div className="error-messages">{ageError}</div>  
         </label>
         <label className='all-label'>Total Cost *
         <input className='inputbox1' type="text" name="totalCost" value={customerData.totalCost} onChange={handleInputChange} />
+        <div className="error-messages">{totalCostError}</div>  
         </label>
       </div>
 
       <div className='container1'>
       <label className='all-label'>Start Date *
         <input className='inputbox1'type="date" name="startDate" value={customerData.startDate} onChange={handleInputChange} />
+        <div className="error-messages">{startDateError}</div>  
         </label>
       </div>
       <div>
-        <button class="right-bottom-button-cancel" onClick={onRequestClose}>Cancel</button>
+        <button class="right-bottom-button-cancel" o onClick={() => {
+            resetErrors(); 
+            onRequestClose();}}>Cancel</button>
       </div>
       <div>
         <button  class="right-bottom-button-save" onClick={handleSave}>Save</button>
